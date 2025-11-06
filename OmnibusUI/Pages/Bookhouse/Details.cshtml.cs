919adace92aa8c5ec874ecc7282c4cbe9553e16a
an-npc <jnea.steiner@gmail.com>
2025-11-06 09:36:55 -0600

    Fix CRUD functions for Book Object

---
+++ OmnibusUI/Pages/Bookhouse/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OmnibusUI.Data;
+using OmnibusUI.Models;
+
+namespace OmnibusUI.Pages.Bookhouse
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly OmnibusUI.Data.AppDbContext _context;
+
+        public DetailsModel(OmnibusUI.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Bookhouse.FirstOrDefaultAsync(m => m.bookID == id);
+
+            if (book is not null)
+            {
+                Book = book;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+    }
+}
